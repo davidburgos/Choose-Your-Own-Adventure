9fde75932d0a5115c3f1e82125338a2359aad386
david.burgos <david.burgos@globant.com>
2015-01-25 00:36:28 -0500

    -launch one of two random screens

--- app/src/main/java/co/mobilemakers/yourownadventure/MainActivity.java
+++ app/src/main/java/co/mobilemakers/yourownadventure/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -20,8 +22,18 @@ public class MainActivity extends ActionBarActivity {
         Start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent Alley_intent = new Intent(MainActivity.this, Alley.class);
-                startActivity(Alley_intent);
+
+                Random rn = new Random();
+
+                if(rn.nextBoolean())
+                {
+                    Intent Alley_intent = new Intent(MainActivity.this, Alley.class);
+                    startActivity(Alley_intent);
+                }else{
+                    Intent Room_intent = new Intent(MainActivity.this, Room.class);
+                    startActivity(Room_intent);
+                }
+
             }
         });
     }
