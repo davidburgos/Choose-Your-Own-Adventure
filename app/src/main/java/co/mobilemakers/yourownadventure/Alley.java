e9106cdf59a8b722447bda487dfce11f29b14088
DavidBurgos <david.burgos@globant.com>
2015-01-26 00:10:16 -0500

    -add a fragments to the project -add functionality to the buttons

--- app/src/main/java/co/mobilemakers/yourownadventure/Alley.java
+++ app/src/main/java/co/mobilemakers/yourownadventure/Alley.java
@@ -1,9 +1,16 @@
 package co.mobilemakers.yourownadventure;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
 
 
 public class Alley extends ActionBarActivity {
@@ -12,6 +19,12 @@ public class Alley extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alley);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
     }
 
 
@@ -35,5 +48,21 @@ public class Alley extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_alley, container, false);
+            return rootView;
+        }
     }
 }
--- app/src/main/java/co/mobilemakers/yourownadventure/Room.java
+++ app/src/main/java/co/mobilemakers/yourownadventure/Room.java
@@ -1,19 +1,66 @@
 package co.mobilemakers.yourownadventure;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+
+import java.util.Random;
 
 
 public class Room extends ActionBarActivity {
+
+    private final static String WIN_MESSAGE = "You've reached the Gold!";
+    private final static String LOOSE_MESSAGE = "You've fallen into the pit of despair";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        Button btnDoor_1 = (Button)findViewById(R.id.btn_door_1);
+        Button btnDoor_2 = (Button)findViewById(R.id.btn_door_2);
+
     }
 
+    public void OnClickButtons(View v){
+
+        Random rn = new Random();
+
+        if(rn.nextBoolean())
+        {
+            if(rn.nextBoolean()){
+                Intent Room_intent = new Intent(Room.this, Room.class);
+                startActivity(Room_intent);
+            }else{
+                Intent Alley_intent = new Intent(Room.this, Alley.class);
+                startActivity(Alley_intent);
+            }
+        }else{
+            Intent GameOverScreen = new Intent(Room.this, GameOver.class);
+
+            if(rn.nextBoolean()){
+                GameOverScreen.putExtra(Intent.EXTRA_TEXT, LOOSE_MESSAGE);
+            }else{
+                GameOverScreen.putExtra(Intent.EXTRA_TEXT, WIN_MESSAGE);
+            }
+
+            startActivity(GameOverScreen);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +82,21 @@ public class Room extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_room, container, false);
+            return rootView;
+        }
     }
 }
